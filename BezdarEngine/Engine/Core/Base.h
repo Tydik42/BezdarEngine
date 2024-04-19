f181c859fc25d1f78b5fabb4016eaf832ab35a27
Garaev Tagir <tgaraev653@gmail.com>
2024-04-19 16:22:22 +0300

    macros fix

--- BezdarEngine/Engine/Core/Base.h
+++ BezdarEngine/Engine/Core/Base.h
@@ -5,6 +5,6 @@
 #define BZ_EXPAND_MACRO(x) x
 #define BZ_STRINGIFY_MACRO(x) #x
 
-#define HZ_DEBUGBREAK() __debugbreak()
+#define BZ_DEBUGBREAK() __debugbreak()
 
 #define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
